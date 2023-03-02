0a9f7bf71f6d74d4d4a0787e97f1e3fcdc953d0d
Bruno Palhano <107354811+jbrunopg@users.noreply.github.com>
2023-03-01 23:06:49 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+import md5 from 'md5'
+
+const baseURL = 'http://gateway.marvel.com/v1/public/comics?';
+
+const publickey = 'caff910f04292e84d04c6714056a2637';
+const privateKey = '536065eba51bd3c854cb049bfda3b88e97883603';
+
+const time = Number(new Date());
+
+const hash = 
+
+const App: React.FC = () => {
+  return <div>Marvel</div>;
+}
+
+export default App;
